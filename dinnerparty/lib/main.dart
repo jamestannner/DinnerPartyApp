f80a0f52fc7e960dfe252c89557ef6c88e751934
James Tanner <jamesboydentanner@gmail.com>
2023-01-06 15:46:57 -0500

    added email verification

--- dinnerparty/lib/main.dart
+++ dinnerparty/lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:dinnerparty/firebase_options.dart';
+import 'package:dinnerparty/views/login_view.dart';
+import 'package:dinnerparty/views/register_view.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
@@ -32,18 +34,41 @@ class HomePage extends StatelessWidget {
         builder: (context, snapshot) {
           switch (snapshot.connectionState) {
             case ConnectionState.done:
-              final user = FirebaseAuth.instance.currentUser;
-              if (user?.emailVerified ?? false) {
-                print('you are verified');
-              } else {
-                print('you are not verified');
-              }
-              return const Text("Done");
+              // final user = FirebaseAuth.instance.currentUser;
+              // if (user?.emailVerified ?? false) {
+              //   return const Text('done');
+              // } else {
+              //   return const VerifyEmailView();
+              // }
+              return LoginView();
             default:
               return const Text('Loading...');
           }
         },
       ),
     );
+  }
+}
+
+class VerifyEmailView extends StatefulWidget {
+  const VerifyEmailView({super.key});
+
+  @override
+  State<VerifyEmailView> createState() => _VerifyEmailViewState();
+}
+
+class _VerifyEmailViewState extends State<VerifyEmailView> {
+  @override
+  Widget build(BuildContext context) {
+    return Column(children: [
+      const Text('Please verify your email address:'),
+      TextButton(
+        onPressed: () async {
+          final user = FirebaseAuth.instance.currentUser;
+          await user?.sendEmailVerification();
+        },
+        child: const Text('Send email verification'),
+      )
+    ]);
   }
 }
--- dinnerparty/lib/views/register_view.dart
+++ dinnerparty/lib/views/register_view.dart
@@ -31,64 +31,48 @@ class _RegisterViewState extends State<RegisterView> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text("Register"),
+    return Column(children: [
+      TextField(
+        controller: _email,
+        enableSuggestions: false,
+        autocorrect: false,
+        keyboardType: TextInputType.emailAddress,
+        decoration: const InputDecoration(
+          hintText: 'Enter your email here',
+        ),
       ),
-      body: FutureBuilder(
-          future: Firebase.initializeApp(
-            options: DefaultFirebaseOptions.currentPlatform,
-          ),
-          builder: (context, snapshot) {
-            switch (snapshot.connectionState) {
-              case ConnectionState.done:
-                return Column(children: [
-                  TextField(
-                    controller: _email,
-                    enableSuggestions: false,
-                    autocorrect: false,
-                    keyboardType: TextInputType.emailAddress,
-                    decoration: const InputDecoration(
-                      hintText: 'Enter your email here',
-                    ),
-                  ),
-                  TextField(
-                      controller: _password,
-                      obscureText: true,
-                      enableSuggestions: false,
-                      autocorrect: false,
-                      decoration: const InputDecoration(
-                        hintText: 'Enter your password here',
-                      )),
-                  TextButton(
-                      onPressed: () async {
-                        final email = _email.text;
-                        final password = _password.text;
-                        try {
-                          final userCredential = await FirebaseAuth.instance
-                              .createUserWithEmailAndPassword(
-                                  email: email, password: password);
-                          print(userCredential);
-                        } on FirebaseAuthException catch (e) {
-                          if (e.code == 'weak-password') {
-                            print('WEAK PASSWORD!');
-                          } else if (e.code == 'email-already-in-use') {
-                            print("EMAIL ALREADY IN USE !!");
-                          } else if (e.code == 'invalid-email') {
-                            print("INVALID EMAIL!");
-                          } else {
-                            print('error!!');
-                            print(e);
-                            print(e.code);
-                          }
-                        }
-                      },
-                      child: const Text('Register'))
-                ]);
-              default:
-                return const Text('Loading...');
+      TextField(
+          controller: _password,
+          obscureText: true,
+          enableSuggestions: false,
+          autocorrect: false,
+          decoration: const InputDecoration(
+            hintText: 'Enter your password here',
+          )),
+      TextButton(
+          onPressed: () async {
+            final email = _email.text;
+            final password = _password.text;
+            try {
+              final userCredential = await FirebaseAuth.instance
+                  .createUserWithEmailAndPassword(
+                      email: email, password: password);
+              print(userCredential);
+            } on FirebaseAuthException catch (e) {
+              if (e.code == 'weak-password') {
+                print('WEAK PASSWORD!');
+              } else if (e.code == 'email-already-in-use') {
+                print("EMAIL ALREADY IN USE !!");
+              } else if (e.code == 'invalid-email') {
+                print("INVALID EMAIL!");
+              } else {
+                print('error!!');
+                print(e);
+                print(e.code);
+              }
             }
-          }),
-    );
+          },
+          child: const Text('Register'))
+    ]);
   }
 }
