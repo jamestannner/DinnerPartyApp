5172a4bd4c03bf87b7e2d624b3efe0a2bdc4fd94
James Tanner <jamesboydentanner@gmail.com>
2023-01-08 09:46:38 -0500

    added email verification navigation

--- dinnerparty/lib/main.dart
+++ dinnerparty/lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:dinnerparty/firebase_options.dart';
 import 'package:dinnerparty/views/login_view.dart';
 import 'package:dinnerparty/views/register_view.dart';
+import 'package:dinnerparty/views/verify_email_view.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
@@ -34,7 +35,17 @@ class HomePage extends StatelessWidget {
       builder: (context, snapshot) {
         switch (snapshot.connectionState) {
           case ConnectionState.done:
-            // final user = FirebaseAuth.instance.currentUser;
+            final user = FirebaseAuth.instance.currentUser;
+            if (user != null) {
+              if (user.emailVerified) {
+                print('Email is verified');
+              } else {
+                return const VerifyEmailView();
+              }
+            } else {
+              return const LoginView();
+            }
+            return const Text('Done');
             // if (user?.emailVerified ?? false) {
             //   return const Text('done');
             // } else {
