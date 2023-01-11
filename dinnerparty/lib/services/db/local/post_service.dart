3a31b341f46c9dfd6d38d3c084d642ebb431254c
James Tanner <jamesboydentanner@gmail.com>
2023-01-11 13:26:14 -0500

    renamed database services directory

--- dinnerparty/lib/services/db/local/post_service.dart
+++ dinnerparty/lib/services/db/local/post_service.dart
@@ -1,4 +1,4 @@
-import 'package:dinnerparty/services/database/local/local_exceptions.dart';
+import 'package:dinnerparty/services/db/local/local_exceptions.dart';
 import 'package:flutter/material.dart';
 import 'package:sqflite/sqflite.dart';
 import 'package:path_provider/path_provider.dart'
